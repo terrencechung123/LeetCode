def57008466c669d16fad0ec9a1628b99ceb7143
terrencechung123 <111016797+terrencechung123@users.noreply.github.com>
2023-10-02 13:50:39 -0700

    Time: 183 ms (60.66%), Space: 17.3 MB (27.87%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -6,10 +6,10 @@ class Solution(object):
         """
         alice = 0
         bob = 0
-        for i in range(1,(len(colors)-1)):
-            if colors[i] == colors [i-1] and colors[i] == colors[i+1]:
+        for i in range(1,len(colors)-1):
+            if colors[i] == colors[i-1] and colors[i] == colors[i+1]:
                 if colors[i] == 'A':
                     alice += 1
                 else:
                     bob +=1
-        return alice - bob >= 1
+        return alice - bob >=1
