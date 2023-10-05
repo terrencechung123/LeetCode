6e4201355a71bfdfc5a8f24b279acdff1587e6fa
terrencechung123 <111016797+terrencechung123@users.noreply.github.com>
2023-10-04 21:36:24 -0700

    Time: 11 ms (91.39%), Space: 13.3 MB (17.92%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -4,9 +4,8 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        num_str = ''.join(str(num) for num in digits)
-        new_num = int(num_str)+1
-        str_new_num = str(new_num)
-        new_number = [int(i) for i in str_new_num]
-        result = new_number
+        string = ''.join(str(digit) for digit in digits)
+        value = int(string)+1
+        value_string = str(value)
+        result = [int(i) for i in value_string]
         return result
