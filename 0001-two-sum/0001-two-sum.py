249466156f95c02b61c73bfe275ec3639413b50e
terrencechung123 <111016797+terrencechung123@users.noreply.github.com>
2023-10-25 02:55:57 -0700

    Time: 2175 ms (36.96%), Space: 13.9 MB (91.15%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -7,5 +7,5 @@ class Solution(object):
         """
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
+                if nums[i]+nums[j] == target:
                     return [i,j]
