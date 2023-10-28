eeb4d548c3748901147b938ca1b2fe182e9600f6
terrencechung123 <111016797+terrencechung123@users.noreply.github.com>
2023-10-28 01:02:36 -0700

    Time: 37 ms (72.00%), Space: 16.2 MB (35.91%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,12 +1,11 @@
 class Solution:
     def addDigits(self, num: int) -> int:
         new_num = str(num)
-        sum = 0
         while True:
+            sum=0
             for i in range(len(new_num)):
                 sum += int(new_num[i])
             if sum < 10:
                 break
             new_num = str(sum)
-            sum=0
         return sum
