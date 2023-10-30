ff7323a5bd978a20bc12b5431c345719564ee637
terrencechung123 <111016797+terrencechung123@users.noreply.github.com>
2023-10-29 21:42:09 -0700

    Time: 54 ms (16.13%), Space: 17.2 MB (95.92%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        arr = []
+        for i in range(n):
+            if (i+1) % 3 == 0 and (i+1) % 5 == 0:
+                arr.append('FizzBuzz')
+            elif (i+1) % 3 == 0:
+                arr.append('Fizz')
+            elif (i+1) % 5 == 0:
+                arr.append('Buzz')
+            else:
+                arr.append(f"{i+1}")
+        return arr
