7b0e874c1d336c64e6d928c025af82fc9a8f6caf
terrencechung123 <111016797+terrencechung123@users.noreply.github.com>
2023-10-29 18:47:48 -0700

    Time: 28 ms (97.97%), Space: 16.1 MB (70.47%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -2,11 +2,11 @@ class Solution:
     def fib(self, n: int) -> int:
         if n == 0:
             return 0
-        if n==1 or n==2:
+        if n == 1 or n == 2:
             return 1
         bottom_up = [None]*(n+1)
         bottom_up[1]=1
         bottom_up[2]=1
         for i in range(3,n+1):
-            bottom_up[i]=bottom_up[i-1]+bottom_up[i-2]
+            bottom_up[i] = bottom_up[i-1] + bottom_up[i-2]
         return bottom_up[n]
