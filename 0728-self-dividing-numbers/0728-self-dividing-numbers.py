fa23a41d9c233a6fe8bb8c058881ea133bab646e
terrencechung123 <111016797+terrencechung123@users.noreply.github.com>
2023-10-31 00:35:50 -0700

    Time: 49 ms (71.70%), Space: 16.2 MB (48.47%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        mySet = set()
+        for i in range(left,right+1):
+            num_str = str(i)
+            isTrue=True
+            for digit in num_str:
+                if int(digit) == 0 or i % int(digit) != 0:
+                    isTrue=False
+                    break
+            if isTrue:
+                mySet.add(i)
+
+        return sorted(mySet)
