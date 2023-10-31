4a4ef67b8051199edadac1fbba51b4c6310fcb69
terrencechung123 <111016797+terrencechung123@users.noreply.github.com>
2023-10-30 17:57:24 -0700

    Time: 37 ms (67.14%), Space: 16.2 MB (70.25%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.py
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.py
@@ -1,18 +1,3 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
-        my_dict = {'A': 0, 'L': 0}
-        for i in range(len(s)):
-            if s[i] in my_dict:
-                if my_dict[s[i]]>=2:
-                    return False
-                if s[i] == 'A':
-                    my_dict[s[i]] += 1
-                if s[i] == 'L':
-                    if i > 0 and s[i] == s[i-1]:
-                        my_dict[s[i]]+=1
-                    else:
-                        my_dict[s[i]]=0
-        for x,y in my_dict.items():
-            if y >= 2:
-                return False
-        return True
+        return s.count('A') < 2 and s.count('LLL') < 1
