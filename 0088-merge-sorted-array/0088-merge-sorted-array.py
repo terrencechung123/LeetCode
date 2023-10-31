e36e18a3f743b0c3320f042812706cb9d686f161
terrencechung123 <111016797+terrencechung123@users.noreply.github.com>
2023-10-30 22:56:48 -0700

    Time: 43 ms (58.48%), Space: 16.1 MB (98.59%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,10 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        new_num = []
         new_num = [nums1[i] for i in range(m)]
         new_num += [nums2[i] for i in range(n)]
         new_num=sorted(new_num)
-        
         nums1.clear()
         nums1.extend(new_num)
