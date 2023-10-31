d9b6459d2b7784b40d9d379fa878bc6c80b8d761
terrencechung123 <111016797+terrencechung123@users.noreply.github.com>
2023-10-30 22:58:44 -0700

    Time: 35 ms (94.68%), Space: 16.4 MB (29.06%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,6 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        new_num = sorted([nums1[i] for i in range(m)] + [nums2[i] for i in range(n)])
-        nums1.clear()
-        nums1.extend(new_num)
+        p1 = m - 1
+        p2 = n - 1
+        # Initialize the pointer for the merged list, starting from the end
+        p = m + n - 1
+
+        # While there are elements left in both lists
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+
+        # If there are remaining elements in nums2, copy them to nums1
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
