1fb96a26ec946636dd6a61e85d5145bbf81abc56
terrencechung123 <111016797+terrencechung123@users.noreply.github.com>
2023-11-06 21:41:16 -0800

    Time: 42 ms (33.19%), Space: 16.2 MB (68.70%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,8 +1,7 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        hashSet = set(jewels)
         count=0
         for stone in stones:
-            if stone in hashSet:
+            if stone in jewels:
                 count+=1
         return count
